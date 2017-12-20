336b7589f25c5424d97fef6c9933b35602591bf4
George <george_whiting@hotmail.com>
2017-12-20 17:15:38 +0000

    adds maximum temperatures for different ecomode states

--- jasmine/spec/thermostatSpec.js
+++ jasmine/spec/thermostatSpec.js
@@ -1,9 +1,10 @@
 describe("thermostat", function() {
 
   var thermostat;
+  var ecomode;
 
   beforeEach(function() {
-    thermostat = new Thermostat
+    thermostat = new Thermostat;
   });
 
   it("should initialise with a temperature of 20", function() {
@@ -16,11 +17,21 @@ describe("thermostat", function() {
     });
     it("shouldn't allow the temperature to go below 10", function() {
       thermostat.temperature = 10;
-      expect(function() {thermostat.down()}).toThrow(new Error("Stop it! I'm freezing!"))
-    })
+      expect(function() {thermostat.down()}).toThrow(new Error("Stop it! I'm freezing!"));
+    });
     it("should be able to turn the temperature down", function() {
       thermostat.down(1);
       expect(thermostat.temperature).toEqual(19);
+    });
+    it("should not go higher than 25 if ecomode is on", function() {
+      ecomode = jasmine.createSpyObj('ecomodeOn', {'isOn': true});
+      thermostat.temperature = 25;
+      expect(function() {thermostat.up(ecomode)}).toThrow(new Error("Don't waste dat energy, fool"));
+    });
+    it("should never go higher than 32", function() {
+      ecomode = jasmine.createSpyObj('ecomodeOff', {'isOn': false});
+      thermostat.temperature = 32;
+      expect(function() {thermostat.up(ecomode)}).toThrow(new Error("Stop it! Mans CAN be too hot"));
     });
   });
 });
--- jasmine/src/thermostat.js
+++ jasmine/src/thermostat.js
@@ -1,7 +1,13 @@
 var Thermostat = function(){
   this.temperature = 20;
 
-  this.up = function() {
+  this.up = function(ecomode = new Ecomode) {
+    if (this.temperature >= 32) {
+      throw new Error("Stop it! Mans CAN be too hot");
+    }
+    else if (ecomode.isOn && this.temperature >= 25) {
+      throw new Error("Don't waste dat energy, fool");
+    };
     this.temperature += 1;
   };
 
