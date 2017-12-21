7ab336bd86ea801026869169257e666fb9382657
George <george_whiting@hotmail.com>
2017-12-21 14:50:40 +0000

    everything is now a prototype function

--- src/thermostat.js
+++ src/thermostat.js
@@ -2,50 +2,6 @@
 var Thermostat = function(){
   this.temperature = 20;
   this.ecomodeIsOn = true;
-
-  this.up = function() {
-    if (this.temperature >= 32) {
-      throw new Error("Stop it! Mans CAN be too hot");
-    }
-    else if (this.ecomodeIsOn && this.temperature >= 25) {
-      throw new Error("Don't waste dat energy, fool");
-    };
-    this.temperature += 1;
-  };
-
-  this.down = function() {
-    if (this.temperature <= 10) {
-      throw new Error("Stop it! I'm freezing!")
-    }
-    this.temperature -= 1;
-  };
-
-  this.reset = function() {
-    this.temperature = 20;
-  };
-
-  this.usageEnquire = function() {
-    switch(true) {
-      case (this.temperature < 18):
-        return "low-usage";
-        break;
-      case (this.temperature < 25 && this.temperature > 18):
-        return "medium-usage";
-        break;
-      case (this.temperature >= 25):
-        return "high-usage";
-        break;
-    };
-  };
-
-  this.ecomodeTurnOff = function() {
-    this.ecomodeIsOn = false;
-  };
-
-  this.ecomodeTurnOn = function() {
-    this.ecomodeIsOn = true;
-  };
-
 };
 
 Thermostat.prototype.getTemp = function() {
@@ -54,4 +10,47 @@ Thermostat.prototype.getTemp = function() {
 
 Thermostat.prototype.isEcomodeOn = function() {
   return this.ecomodeIsOn === true;
+};
+
+Thermostat.prototype.ecomodeTurnOff = function() {
+  this.ecomodeIsOn = false;
+};
+
+Thermostat.prototype.ecomodeTurnOn = function() {
+  this.ecomodeIsOn = true;
+};
+
+Thermostat.prototype.up = function() {
+  if (this.temperature >= 32) {
+    throw new Error("Stop it! Mans CAN be too hot");
+  }
+  else if (this.ecomodeIsOn && this.temperature >= 25) {
+    throw new Error("Don't waste dat energy, fool");
+  };
+  this.temperature += 1;
+};
+
+Thermostat.prototype.down = function() {
+  if (this.temperature <= 10) {
+    throw new Error("Stop it! I'm freezing!")
+  }
+  this.temperature -= 1;
+};
+
+Thermostat.prototype.reset = function() {
+  this.temperature = 20;
+};
+
+Thermostat.prototype.usageEnquire = function() {
+  switch(true) {
+    case (this.temperature < 18):
+      return "low-usage";
+      break;
+    case (this.temperature < 25 && this.temperature > 18):
+      return "medium-usage";
+      break;
+    case (this.temperature >= 25):
+      return "high-usage";
+      break;
+  };
 };
