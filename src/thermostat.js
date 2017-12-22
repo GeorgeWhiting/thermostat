6dce076606d6659ed66963218e29c7493c4f124e
George <george_whiting@hotmail.com>
2017-12-22 12:04:06 +0000

    adds coloured text for temperature

--- src/thermostat.js
+++ src/thermostat.js
@@ -6,7 +6,7 @@ var Thermostat = function(){
   this.MEDIUM_USAGE_LIMIT = 18;
   this.MINIMUM_TEMPERATURE = 10;
   this.temperature = this.DEFAULT_TEMPERATURE;
-  this.ecomodeIsOn = true;
+  this.ecomodeIsOn = 'on';
 };
 
 Thermostat.prototype.getTemp = function() {
@@ -14,25 +14,26 @@ Thermostat.prototype.getTemp = function() {
 };
 
 Thermostat.prototype.isEcomodeOn = function() {
-  return this.ecomodeIsOn === true;
+  return this.ecomodeIsOn === 'on';
 };
 
 Thermostat.prototype.ecomodeTurnOff = function() {
-  this.ecomodeIsOn = false;
+  this.ecomodeIsOn = 'off';
 };
 
 Thermostat.prototype.ecomodeTurnOn = function() {
-  this.ecomodeIsOn = true;
+  this.ecomodeIsOn = 'on';
   if (this.getTemp() > this.ECOMODE_MAX) {
     this.temperature = this.ECOMODE_MAX;
   };
 };
 
 Thermostat.prototype.up = function() {
+
   if (this.temperature >= this.NO_ECOMODE_MAX) {
     throw new Error("Stop it! Mans CAN be too hot");
   }
-  else if (this.ecomodeIsOn && this.temperature >= this.ECOMODE_MAX) {
+  else if ((this.ecomodeIsOn === 'on') && (this.temperature >= this.ECOMODE_MAX)) {
     throw new Error("Don't waste dat energy, fool");
   };
   this.temperature ++;
@@ -59,6 +60,20 @@ Thermostat.prototype.usageEnquire = function() {
       break;
     case (this.temperature >= this.ECOMODE_MAX):
       return "high-usage";
+      break;
+  };
+};
+
+Thermostat.prototype.textColour = function() {
+  switch(true) {
+    case (this.temperature < this.MEDIUM_USAGE_LIMIT):
+      return "green";
+      break;
+    case (this.temperature < this.ECOMODE_MAX && this.temperature > this.MEDIUM_USAGE_LIMIT):
+      return "black";
+      break;
+    case (this.temperature >= this.ECOMODE_MAX):
+      return "red";
       break;
   };
 };
